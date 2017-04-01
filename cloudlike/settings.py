1f74ea48bcb64905e2746eb33f6ea380d38be189
m <mmailm@mail.ru>
2017-04-01 14:50:13 +0800

    add form bootstrap

--- cloudlike/settings.py
+++ cloudlike/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'bootstrapform',
     'repository',
 ]
 
@@ -106,7 +107,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
